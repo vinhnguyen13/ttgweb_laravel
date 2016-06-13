e79b6910e3741e3f52a04a278f8faec486a6c9a4
vinhnguyen13 <quangvinh.nguyen@trungthuygroup.vn>
2016-06-13 15:31:08 +0700

    ide helper

--- web/config/app.php
+++ web/config/app.php
@@ -155,6 +155,10 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        /*
+         * IDE helper
+         */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
     ],
 
